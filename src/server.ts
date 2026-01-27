337f9e9967048ba5c2507c32dc5413054bbb7bd7
conghieu <conghieu@vnpt.vn>
2026-01-28 00:08:41 +0700

    cors update

--- src/server.ts
+++ src/server.ts
@@ -11,8 +11,10 @@ const app: Application = express();
 const port = env.port;
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:4200", "https://back-lu4k.vercel.app"],
+    origin: '*',
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }))
 
 app.listen(port, () => {
